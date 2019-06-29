9c6dedba67687dda1119aa0dc774a3e170c3c0e6
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-29 02:12:01 +0000

    started welcome page

--- lib/my_cli_got.rb
+++ lib/my_cli_got.rb
@@ -1,6 +1,43 @@
 class GOT
-  def run
-    puts 'the GOT is running!'
-  end
+  def welcome
+    puts ""
+    puts"       |>      |>                     |>      |>"
+    puts"       |_-___-_|                      |_-___-_|"
+    puts"        |vvvvv|                        |vvvvv|"
+    puts"       |__[[]]_|                      |_[[]]__|"
+    puts"      |   |**|  |_-_-_-_-_-_-_-_-_-_-_| |**|  |"
+    puts"     |    |  |   vvvvvvvvvvvvvvvvvvvvv  |  |   |"
+    puts"     |    |XX|          GAME OF         |XX|   |"
+    puts"     |    |()|          THRONES         |()|   |"
+    puts"     |     \/      vvvvvvvvvvvvvvvvv     \/    |"
+    puts"     |                  +++++                  |"
+    puts"     |                _/     \_                |"
+    puts"     |               _|   O   |_               |"
+    puts"     |               _|  ooo  |_               |"
+    puts"     |mmmmmmmmmmmmmmmm|       |mmmmmmmmmmmmmmmm|"
+    puts ""
+    puts "To see the list of the great houses of Westeros, type: list.".bold 
+    puts "To see the information of all the houses of Westeros, type: all".bold
+  end 
+  
+  def list_of_house
+end
+
+  def general_info
+end
+
+  def menu
+end
+ 
+  def sigils
+end
+
+def motto
+end
+
+
+
+
+
 end
 
