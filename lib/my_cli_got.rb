839c3b7a77aed7510ba50aa4a2922f34a12f0fd7
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-29 23:25:46 +0000

    welcome page work1

--- lib/my_cli_got.rb
+++ lib/my_cli_got.rb
@@ -1,23 +1,35 @@
+require 'nokogiri'
+require 'colorize'
+
 class GOT
+  
+  def run
+    welcome
+  end
+  
   def welcome
     puts ""
-    puts"       |>      |>                     |>      |>"
-    puts"       |_-___-_|                      |_-___-_|"
-    puts"        |vvvvv|                        |vvvvv|"
-    puts"       |__[[]]_|                      |_[[]]__|"
-    puts"      |   |**|  |_-_-_-_-_-_-_-_-_-_-_| |**|  |"
-    puts"     |    |  |   vvvvvvvvvvvvvvvvvvvvv  |  |   |"
-    puts"     |    |XX|          GAME OF         |XX|   |"
-    puts"     |    |()|          THRONES         |()|   |"
-    puts"     |     \/      vvvvvvvvvvvvvvvvv     \/    |"
-    puts"     |                  +++++                  |"
-    puts"     |                _/     \_                |"
-    puts"     |               _|   O   |_               |"
-    puts"     |               _|  ooo  |_               |"
-    puts"     |mmmmmmmmmmmmmmmm|       |mmmmmmmmmmmmmmmm|"
+    puts"       |>      |>                     |>      |>".bold
+    puts"       |_-___-_|                      |_-___-_|".bold
+    puts"        |vvvvv|                        |vvvvv|".bold
+    puts"       |__[[]]_|                      |_[[]]__|".bold
+    puts"      |   |**|  |_-_-_-_-_-_-_-_-_-_-_| |**|  |".bold
+    puts"     |    |  |   vvvvvvvvvvvvvvvvvvvvv  |  |   |".bold
+    puts"     |    |XX|          GAME OF         |XX|   |".bold
+    puts"     |    |()|          THRONES         |()|   |".bold
+    puts"     |     \/      vvvvvvvvvvvvvvvvv     \/    |".bold
+    puts"     |                  +++++                  |".bold
+    puts"     |                _/     \_                |".bold
+    puts"     |               _|   O   |_               |".bold
+    puts"     |               _|  ooo  |_               |".bold
+    puts"     |mmmmmmmmmmmmmmmm|       |mmmmmmmmmmmmmmmm|".bold
     puts ""
-    puts "To see the list of the great houses of Westeros, type: list.".bold 
-    puts "To see the information of all the houses of Westeros, type: all".bold
+    puts "                    W E L C O M E                  ".bold
+    puts "         TO THE BOOK ABOUT THE GREAT HOUSES        ".bold
+    puts "                     OF WESTEROS                    ".bold
+    puts ""
+    puts "To see the list of the great houses of Westeros, type: " + "list".bold 
+    puts "To see the information of all the houses of Westeros, type: " + "all".bold
   end 
   
  def list_of_house
@@ -25,8 +37,14 @@ class GOT
     if input== "list"
       list = Scraper.house.text.split("House")
       list.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-    end
-    list
+     elsif input == "exit"
+    system "clear" or sys "cls"
+    exit
+  else
+    puts "The Lord Command Snow knows nothing of this order...Try again".bold
+    
+  end
+    
 end
 
   def general_info
@@ -35,12 +53,12 @@ end
       all = Scraper.house.text.split("\n")
       all.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
     elsif input == "exit"
-    system "clear" or sys "cls"
+    system "clear"
     exit
   else
-    puts "give me something to work..Try again".bold
+    puts "The Lord Command Snow knows nothing of this order...Try again".bold
     
-  def menu
+  end
 end
  
 
