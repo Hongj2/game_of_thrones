acd516ffdb2651f671ea269335641eb9482ab9ce
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-01 23:16:07 +0000

    welcome page4

--- lib/my_cli_got.rb
+++ lib/my_cli_got.rb
@@ -8,7 +8,6 @@ class GOT
     orders
   end
   
-  #issue = currently it requires user to follow the run order.. if 'motto' was first, it does not reconize the motto method 
   
   def welcome
     puts ""
@@ -31,9 +30,10 @@ class GOT
     puts "         TO THE BOOK ABOUT THE GREAT HOUSES        ".bold
     puts "                     OF WESTEROS                    ".bold
     puts ""
-    puts "To see the list of the great houses of Westeros, type: " + "list".red.bold 
-    puts "To see the information of all the houses of Westeros, type: " + "all".red.bold
-   puts "To see the mottos of all the houses of Westeros, type: " + "motto".red.bold
+    puts "To see the list of the great houses of Westeros, type: " + "[list]".red.bold 
+    puts "To see the information of all the houses of Westeros, type: " + "[all]".red.bold
+    puts "To see the mottos of all the houses of Westeros, type: " + "[mottos]".red.bold
+    puts "To see the sigils of all the houses of Westeros, type: " + "[sigils]".red.bold
   end 
   
   def orders
@@ -42,34 +42,149 @@ class GOT
       list_of_house
     elsif input == "all"
       general_info
-    elsif  input == "motto"
+    elsif  input == "mottos"
       mottos
+    elsif input == "sigils"
+      sigils
+    elsif  input == "menu"
+      pathways
     else 
-      puts "The Lord Command Snow knows nothing of this order...Try again".blue.bold
+      puts "The Lord Command Snow knows nothing...Try again".blue.bold
     end
   end
   
   
- def list_of_house
-     puts "hi list"
-     #list = Scraper.house.text.split("House")
-      #list.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-      
- end
+  def pathways
+    puts ""
+    puts "To exit type: [exit]"
+    puts "To return back to the main page type: [welcome]"
+    puts "To see the list of the great houses of Westeros, type: " + "[list]".red.bold 
+    puts "To see the information of all the houses of Westeros, type: " + "[all]".red.bold
+    puts "To see the mottos of all the houses of Westeros, type: " + "[motto]".red.bold      
+     input= gets.strip
+    if input == "welcome"
+      run
+    elsif  input == "mottos"
+      mottos
+    elsif input == "list"
+      list_of_house
+    elsif input == "all"
+      general_info
+    elsif  input == "motto"
+      mottos
+    elsif input == "exit"
+     exit
+    else 
+      puts "The Lord Command Snow knows nothing...Try again".blue.bold
+      pathways
+    end
+  
+  end
 
   def general_info
-   puts "hi all"
-     # all = Scraper.house.text.split("\n")
-     # all.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-
+    puts "general list of information"
+     pathways
   end
+      
+
+  
+  
+ def list_of_house
+     puts "[PUTS LIST OF HOUSE METHOD TO OUTPUT A LIST WITH INDEX NUMBER !!]"
+     puts ""
+     puts "To see the general information about a house, type number [1-20]"
+     puts "To see menu type [menu]"
+     
+      input= gets.strip
+     if input.to_i.to_s == input
+         puts "should list a specific house information #{input} [NEEDS A METHOD [y] !!]"
+         puts ""
+         puts "To see the wiki page link type: [geek out]"
+         puts "to return back to the list of houses types: [list]"
+         puts "To see the general information about another house, type number [1-20]"
+        
+               input= gets.strip
+                 if input.to_i.to_s == input
+                     puts "should list a specific house information #{input} [NEEDS A METHOD [y] !!]"
+                 elsif input == "geek out"
+                     puts "[PUTS WIKI PAGE LINK METHOD]"
+                 elsif  input == "list"
+                    list_of_house
+                 else 
+                      puts "The Lord Command Snow knows nothing...Try again".blue.bold
+                  end
+                  pathways
+     elsif input == "menu"
+         pathways
+     end
+    
+ end
+
+
+
+
  
   def mottos
-     puts "hi motto"
-     # all = Scraper.house.text.split("\n")
-     # all.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-   
-  end
+     puts "[PUTS MOTTOS METHOD TO OUTPUT A LIST WITH INDEX NUMBER !!] "
+     puts ""
+     puts "To see the general information about a house, type number [1-20]"
+     puts "To see menu type [menu]"
+     
+      input= gets.strip
+      if input.to_i.to_s == input
+         puts "should list a specific house information #{input} [NEEDS A METHODX [y] !!]"
+         puts ""
+         puts "To see the wiki page link type: [geek out]"
+         puts "To return back to the list of mottos types: [mottos]"
+    
+               input= gets.strip
+                 if input == "mottos"
+                      mottos
+                 elsif input == "geek out"
+                      puts "[PUTS WIKI PAGE LINK METHOD!!]"
+                 else 
+                      puts "The Lord Command Snow knows nothing...Try again".blue.bold
+                    pathways
+                  end
+                  
+     elsif input == "menu"
+         pathways
+     end
+    
+ end
+  
+  
+ 
+  def sigils
+     puts "[PUTS SIGILS METHOD TO OUTPUT A LIST WITH INDEX NUMBER !!] "
+     puts ""
+     puts "To see the general information about a house, type number [1-20]"
+     puts "To see menu type [menu]"
+     
+      input= gets.strip
+      if input.to_i.to_s == input
+         puts "should list a specific house information #{input} [NEEDS A METHODX [y] !!]"
+         puts ""
+         puts "To see the wiki page link type: [geek out]"
+         puts "To return back to the list of sigils, types: [sigils]"
+    
+               input= gets.strip
+                 if input == "sigils"
+                      sigils
+                 elsif input == "geek out"
+                      puts "[PUTS WIKI PAGE LINK METHOD!!]"
+                 else 
+                      puts "The Lord Command Snow knows nothing...Try again".blue.bold
+                    pathways
+                  end
+                  
+     elsif input == "menu"
+         pathways
+     end
+    
+ end
+  
+  
 
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ class Scraper
   BASE_URL = "https://gameofthrones.fandom.com/wiki/Great_House?oldid=417926"
   
   def
+end
+
+end
+
+#LIST OF ALL THE HOUSES 
+  #list = Scraper.house.text.split("House")
+  #list.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
+    
+    
+#GENERAL INFOMATION OF ALL THE HOUSES
+  # all = Scraper.house.text.split("\n")
+  # all.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
