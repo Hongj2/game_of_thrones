f6c15b2ea151b03a12aa365f9e9bf10685dc6035
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-10 16:16:54 +0000

    scaper and welcome page w office hour

--- lib/my_cli_got.rb
+++ lib/my_cli_got.rb
@@ -8,7 +8,7 @@ class GOT
  
  
   def run
-    Scraper.scrape_lvl1 
+    Scraper.scraper_gen_info
     welcome
     orders
   end
@@ -41,14 +41,14 @@ class GOT
 
 
 
-def tester
+def get_house_detail(input)
+  input=input.to_i
   
-    input = gets.chomp.to_i
-    if (1..House.all.length).include?(input)
-    selected_house = puts House.all[input -1]
+    if (1..House.all.length).to_a.include?(input)
+    selected_house = House.all[input -1]
       Scraper.general_information(selected_house)
     end
-  exit 
+   
 end
 
 
@@ -117,105 +117,34 @@ House.all.each_with_index {|house,index| puts "[#{index+1}] #{house.name}"}
   puts ""
      
   input= gets.strip
-    if input.to_i.to_s == input
-      ## using House.all find the corresponding house instance
-      #select_house = House.all[input]
-      #Scraper.gen(select_house)
-      puts ""
-      puts "To see menu type:" "[menu]".blue.bold 
-      puts "to return back to the list of houses types:" "[list]".blue.bold 
-      puts ""
-          input= gets.strip
-            if input == "list"
-                list_of_house
-             elsif input == 'menu'
-                pathways
-             else 
-              puts "The Lord Command Snow knows nothing...Try again".blue.bold
-              puts ""
-                pathways
-            end
-     elsif input == "menu"
+  get_house_detail(input)
+  
+    #if input.to_i.to_s == input
+    
+      # puts ""
+      # puts "To see menu type:" "[menu]".blue.bold 
+      # puts "to return back to the list of houses types:" "[list]".blue.bold 
+      # puts ""
+      #     input= gets.strip
+      #       if input == "list"
+      #           list_of_house
+      #       elsif input == 'menu'
+      #           pathways
+      #       else 
+      #         puts "The Lord Command Snow knows nothing...Try again".blue.bold
+      #         puts ""
+      #           pathways
+      #       end
+     #elsif input == "menu"
          pathways
-     end
+    # end
 end
 
 
 
 
  
-  #def mottos
-  #   puts ""
-  #   puts "This is a list of mottos of the Great Houses of Westeros".blue.bold
-  #   puts ""
-  #   puts "[PUTS MOTTOS METHOD TO OUTPUT A LIST WITH INDEX NUMBER !!] "
-  #   puts ""
-  #   puts "To see the general information about a house, type number [1-20]"
-  #   puts "To see menu type [menu]"
-  #  puts ""
-     
-  #     input= gets.strip
-  #    if input.to_i.to_s == input
-  #     Scraper.gen(input.to_i)
-  #       puts ""
-  #      puts "To return back to the list of mottos types: [mottos]"
-  #       puts "To see menu type [menu]"
-  #       puts ""
-    
-  #             input= gets.strip
-  #               if input == "mottos"
-  #                    mottos
-  #                    
-  #                  elsif input == 'menu'
-  #                pathways
-  #               else 
-  #                    puts "The Lord Command Snow knows nothing...Try again".blue.bold
-  #                    puts ""
-  #                  pathways
-  #                end
-                  
-  #   elsif input == "menu"
-  #       pathways
-  #   end
-    
- #end
- 
-#  def sigil
-#     puts ""
-#     puts "This is a list of sigils of the Great Houses of Westeros".blue.bold
-#     puts ""
-#     Scraper.scraper_house_wiki
-#     puts ""
-#     puts "To see the general information about a house, type number [1-20]"
-#     puts "To see menu type [menu]"
-#     puts ""
-     
-      
-#      input= gets.strip
-#      if input.to_i.to_s == input
-#       Scraper.gen(input.to_i)
-#         puts ""
-#         puts "To return back to the list of sigils, types: [sigils]"
-#         puts "To see menu type [menu]"
-#         puts ""
-    
- #              input= gets.strip
-#                 if input == "sigil"
- #                     sigil
-#                  elsif input == "menu"
-#                 pathways
-#                 else 
-#                      puts "The Lord Command Snow knows nothing...Try again".blue.bold
-#                      puts ""
-#                    pathways
-#                  end
-                  
-#     elsif input == "menu"
-#         pathways
-#     
-#     end
 
- #end
   
   
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,10 @@ BASE_URL = 'https://gameofthrones.fandom.com/wiki/Great_House'
 
 def self.scraper_gen_info
   page = Nokogiri::HTML(open(BASE_URL)) 
-  general_info = page.css('div#mw-content-text ul')[0..2].text.split("\n")
+  general_info = page.css('div#mw-content-text ul')[0..2].css('b a').each {|houses|
+    name= houses.text
+    url= "https://gameofthrones.fandom.com#{houses.attribute('href').value}" 
+  House.create(name,url)}
   end
   
 #instantiate new House objects with name and url
@@ -25,13 +28,12 @@ end
 #house_obj.sigil = page.css("sigil selector")
 
 def self.general_information(house_obj)
-  
 gen_info= {}
 html = Nokogiri::HTML(open("#{house_obj.url}"))
-      gen_info[:summary]= html.css('div#mw-content-text>p').first.text
-      gen_info[:motto]= html.css('aside div.pi-data-value>a')[1].text
-      gen_info[:sigil]= html.css("div.pi-item>div").first.text 
-gen_info
+      house_obj.summary= html.css('div#mw-content-text>p').first.text
+      house_obj.motto= html.css('aside div.pi-data-value>a')[1].text
+      house_obj.sigil= html.css("div.pi-item>div").first.text 
+house_obj
 binding.pry
 end
  
